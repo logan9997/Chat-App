aa0a28a48ab1402deedf1f6faa2982b9920ae2c4
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-07-10 09:20:11 +0100

    Message model created, renamed chatroom to room

--- App/models.py
+++ App/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class ChatRoom(models.Model):
+class Room(models.Model):
     room = models.AutoField(primary_key=True)
     room_name = models.CharField(max_length=30)
     capacity = models.IntegerField()
@@ -10,3 +10,10 @@ class User(models.Model):
     user = models.AutoField(primary_key=True)
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
+
+
+class Message(models.Model):
+    message = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    message = models.CharField(max_length=150)
