100c965b8c83eaa7281826434e3fa91a6eabe310
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-07-10 09:17:20 +0100

    ChatRoom and User models created

--- App/models.py
+++ App/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class ChatRoom(models.Model):
+    room = models.AutoField(primary_key=True)
+    room_name = models.CharField(max_length=30)
+    capacity = models.IntegerField()
+
+
+class User(models.Model):
+    user = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
