094382177ad08f9e0be395014a72a782bf54f81e
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-07-13 10:09:33 +0100

    login and logout added to urlpatterns

--- App/urls.py
+++ App/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import (
+    home, login, logout
+)
 
 urlpatterns = [
-    path('', home.home, name='home')
+    path('', home.home, name='home'),
+    path('login/', login.login, name='login'),
+    path('logout/', logout.logout, name='logout')
 ]
