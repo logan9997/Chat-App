4da27b587a70cbe94826541314715cc365325de6
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-07-13 14:21:53 +0100

    only using Message model, user renamed to name

--- App/models.py
+++ App/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 
-class Room(models.Model):
-    room_id = models.AutoField(primary_key=True)
-    room_name = models.CharField(max_length=30)
-    capacity = models.IntegerField()
-
-
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-
-
 class Message(models.Model):
     message_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
     message = models.CharField(max_length=150)
