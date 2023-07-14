66049f09bebcdc6d5919a6741046a6f6ccf182ed
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-07-14 12:13:34 +0100

    changed max_length of message to 500

--- App/models.py
+++ App/models.py
@@ -3,4 +3,6 @@ from django.db import models
 class Message(models.Model):
     message_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
-    message = models.CharField(max_length=150)
+    message = models.CharField(max_length=500)
+    datetime_sent = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    
