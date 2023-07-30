01d84e08dee6689deec904c470b0ce587a5a2056
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-07-30 19:23:13 +0100

    datetime sent changed to two seperate fields; date_sent and time_snet

--- App/models.py
+++ App/models.py
@@ -5,5 +5,5 @@ class Message(models.Model):
     message_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=NAME_MAX_LENGTH)
     message = models.CharField(max_length=MESSAGE_MAX_LENGTH)
-    datetime_sent = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    
+    date_sent = models.DateField(auto_now_add=True, null=True, blank=True)
+    time_sent = models.TimeField(auto_now_add=True, null=True, blank=True)
