4af4cd83e0f0609989f07b77a31e15fcccd94b24
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-07-30 19:21:20 +0100

    functions for items, values (for dict objects), and created date_format

--- App/templatetags/tags.py
+++ App/templatetags/tags.py
@@ -1,4 +1,6 @@
 from django import template
+from datetime import datetime
+from ..config import DATE_FORMAT
 
 register = template.Library()
 
@@ -10,3 +12,15 @@ def capitalise(string:str) -> str:
 @register.filter()
 def get(_dict:dict, key:str):
     return _dict.get(key)
+
+@register.filter()
+def values(_dict:dict):
+    return _dict.values()
+
+@register.filter()
+def items(_dict:dict):
+    return _dict.items()
+
+@register.filter
+def date_format(date:datetime, format_string:str):
+    return datetime.strptime(date, DATE_FORMAT).strftime(format_string)
